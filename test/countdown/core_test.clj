34a53d4fb8c63ded5364ec73730f2089d7414901
JPol <jonathan.polin@gmail.com>
2016-12-30 20:25:53 -0800

    Add basic unit tests.

--- test/countdown/core_test.clj
+++ test/countdown/core_test.clj
@@ -4,11 +4,18 @@
             [clojure.test :refer :all]
             [countdown.core :refer :all]))
 
-(testing "check-solution"
-  (testing "valid solutions are valid"
-  (is (check-solution 2 '[+ 1 1]))
-  (is (check-solution 6 '[+ 1 [+ 2 3]])))
-  (testing "can't run arbitrary code"
-  (is 
-       ;;TODO: check file hasn't been written, or remove that part
-      (nil? (check-solution 2 '[do [binding [*out* [io/writer [io/resource "hax.txt"] :append true]]] [print "FAILED"] "FAILED"])))))
+(deftest test-check-solution
+  (testing "check-solution"
+    (testing "valid solutions are valid"
+      (is (check-solution 2 '[+ 1 1]))
+      (is (check-solution 6 '[+ 1 [+ 2 3]])))
+    (testing "invalid solutions are invalid, return nil"
+      (are [answer solution] (nil? (check-solution answer solution))
+           2 [- 1 1]
+           6 [- 1 [+ 2 3]]))
+    (testing "can't run arbitrary code"
+      (is 
+        (nil? (let [state (atom nil)]
+                (do
+                  (check-solution 2 '[swap! state 0xdeadbeef])
+                  @state)))))))
