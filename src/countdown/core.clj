817d73d0275504a924020379f308c8d8b2562a38
JPol <jonathan.polin@gmail.com>
2017-01-18 10:50:14 -0800

    Work towards a game loop.

--- src/countdown/core.clj
+++ src/countdown/core.clj
@@ -1,5 +1,6 @@
 (ns countdown.core
   (:require [clojure
+             [edn :as edn]
              [pprint :refer [pprint]]
              [spec :as s]
              [walk :refer [postwalk]]]
@@ -22,16 +23,83 @@
   (first (filter (every-pred integer? pos?)
                  (map #(% n m) (shuffle ops)))))
 
-(defn goal [xs]
+(defn gen-target [xs]
   (let [ops [+ - * /]]
     (reduce (partial operate-rand ops)
                          ;; use between 2 and 6 (all) of the numbers
                          (take (+ 2 (rand-int 5))
                                (shuffle xs)))))
 
-(defn check-solution [goal solution]
+(defn check-solution [target solution]
   (when (s/valid? :countdown.spec/prefix-solution solution)
     (->> solution
          (postwalk #(if (sequential? %) (seq %) %))
          eval
-         (= goal))))
+         (= target))))
+
+(defn user-input
+  ([] (user-input "exit"))
+  ([exit] (let [in (read-line)]
+            (when (not= exit in)
+              in))))
+
+(defn validate-input-int! []
+  (loop []
+    (try
+      (Integer/parseInt (user-input))
+      (catch java.lang.NumberFormatException _
+        (do
+          (println "That's not a valid number. Try again?")
+          (recur (user-input)))))))
+
+(defn validate-num-big!
+  "Validate user's input is a valid number
+  of big numbers to have."
+  [pred]
+  (loop [in (validate-input-int!)]
+    (let [num-big (parse-int in)]
+        (if (< 0 num-big 5)
+          num-big
+          (do
+            (println
+              "You're allowed between 1 and 4 big numbers (inclusive)")
+            (recur (validate-input-int!)))))))
+
+(defn -main []
+  (loop [input ""
+         game-state {:stage :begin
+                     :score 0}]
+    (when input
+      (case (:stage game-state)
+        :begin (do
+                 (println "LET'S PLAY COUNTDOWN!!")
+                 (println "How many big ones?")
+                 (recur (validate-num-big!)
+                        (conj game-state
+                              {:stage :gen-numbers})))
+        :gen-numbers (let [nums (numbers input)
+                           target (gen-target nums)]
+                       (println (format "Alright, %d big and %d little:"
+                                        input
+                                        (- 6 input)))
+                       (println nums)
+                       (println "And your target:")
+                       (println target)
+                       ;; TODO: do something for the timer
+                       (println "What did you get?")
+                       (recur (validate-input-int!)
+                              (conj game-state
+                                    {:stage :declare-answer
+                                     :nums nums
+                                     :target target})))
+        :declare-answer (do
+                          (println "Alright, walk us through it.")
+                          (recur (comp edn/read-string user-input)
+                                 (conj game-state
+                                       {:stage :check-solution
+                                        :user-num input})))
+        :check-solution (if (check-solution (:user-num game-state) input)
+                          (let [{:keys [user-num target]} game-state]
+                            (println "It's right.")
+                            (case (- target user-num))))
+        :next-thing-placeholder nil))))
